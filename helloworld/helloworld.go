369a335dc7e5f786f64525c97b1d5a96e821b812
siuramka <marius.kamarauskas9999@gmail.com>
2024-08-18 13:43:21 +0300

    initial

---
+++ helloworld/helloworld.go
@@ -0,0 +1,11 @@
+package helloworld
+
+import "fmt"
+
+func Hello() string {
+	return "Hello, world"
+}
+
+func main() {
+	fmt.Println(Hello())
+}
---
+++ helloworld/hellworld_test.go
@@ -0,0 +1,21 @@
+package helloworld
+
+import "testing"
+
+func TestHello(t *testing.T) {
+	got := Hello()
+	want := "Hello, world"
+
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
+}
+
+func TestHello2(t *testing.T) {
+	got := Hello()
+	want := "Hello, world"
+
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
+}
