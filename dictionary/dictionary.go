fcc7f27787bb4da06ac492c8eca9f86da5cc5220
siuramka <marius.kamarauskas9999@gmail.com>
2024-08-19 23:06:46 +0300

    other: finish maps

--- dictionary/dictionary.go
+++ dictionary/dictionary.go
@@ -2,21 +2,69 @@ package dictionary
 
 import "errors"
 
-type Word string
-type Description string
+type Dictionary map[string]string
 
-type Dictionary map[Word]Description
+type DictError string
 
-const wordNotFound = "word not found"
+func (e DictError) Error() string {
+	return string(e)
+}
 
-func (d Dictionary) Search(word Word) (Description, error) {
+const (
+	ErrMsgWordNotFound = "word not found"
+	ErrMsgWordExists   = "word already exists"
+)
+
+var (
+	ErrWordNotFound = DictError(ErrMsgWordNotFound)
+	ErrWordExists   = DictError(ErrMsgWordExists)
+)
+
+func (d Dictionary) Search(word string) (string, error) {
 	desc, ok := d[word]
+
 	if !ok {
-		return "", errors.New(wordNotFound)
+		return "", ErrWordNotFound
 	}
+
 	return desc, nil
 }
 
-func (d Dictionary) Add(word Word, desc Description) {
-	d[word] = desc
+func (d Dictionary) Add(word string, desc string) error {
+	_, searchErr := d.Search(word)
+
+	switch {
+	case errors.Is(searchErr, ErrWordNotFound):
+		d[word] = desc
+	case searchErr == nil:
+		return ErrWordExists
+	}
+
+	return nil
+}
+
+func (d Dictionary) Update(word string, desc string) error {
+	_, searchErr := d.Search(word)
+
+	switch {
+	case errors.Is(searchErr, ErrWordNotFound):
+		return ErrWordNotFound
+	case searchErr == nil:
+		d[word] = desc
+	}
+
+	return nil
+}
+
+func (d Dictionary) Delete(word string) error {
+	_, searchErr := d.Search(word)
+
+	switch {
+	case errors.Is(searchErr, ErrWordNotFound):
+		return ErrWordNotFound
+	case searchErr == nil:
+		delete(d, word)
+	}
+
+	return nil
 }
--- dictionary/dictionary_test.go
+++ dictionary/dictionary_test.go
@@ -7,8 +7,8 @@ func TestSearch_FoundWord(t *testing.T) {
 
 	got, _ := dictionary.Search("test")
 
-	want := Description("this is just a test")
-	AssertSearch(t, got, want)
+	want := "this is just a test"
+	assertEquals(t, got, want)
 }
 
 func TestSearch_NotFoundWord(t *testing.T) {
@@ -16,33 +16,95 @@ func TestSearch_NotFoundWord(t *testing.T) {
 
 	_, err := dictionary.Search("nonExistingWord")
 
-	AssertError(t, err, wordNotFound)
+	assertError(t, err, ErrMsgWordNotFound)
 }
 
-func TestAdd_AddsEntry(t *testing.T) {
+func TestAdd_AddEntry(t *testing.T) {
 	dictionary := make(Dictionary)
-	const word = Word("joe")
-	const desc = Description("momma")
+	word := "joe"
+	desc := "momma"
 
-	dictionary.Add(word, desc)
+	err := dictionary.Add(word, desc)
 
 	got, _ := dictionary.Search(word)
-	AssertSearch(t, got, desc)
+	assertNoError(t, err)
+	assertEquals(t, got, desc)
 }
 
-func AssertSearch(t testing.TB, got, want Description) {
+func TestAdd_DontAddExisingEntry(t *testing.T) {
+	word := "joe"
+	desc := "momma"
+	dictionary := Dictionary{word: desc}
+
+	err := dictionary.Add(word, desc)
+
+	assertError(t, err, ErrMsgWordExists)
+}
+
+func TestUpdate_UpdatesExistingWord(t *testing.T) {
+	word := "joe"
+	desc := "momma"
+	dictionary := Dictionary{word: desc}
+	newDesc := "cool"
+
+	err := dictionary.Update(word, newDesc)
+
+	got, _ := dictionary.Search(word)
+	assertNoError(t, err)
+	assertEquals(t, got, newDesc)
+}
+
+func TestUpdate_ErrWhenUpdateNotExistingWord(t *testing.T) {
+	dictionary := Dictionary{"joe": "momma"}
+	newDesc := "cool"
+	notExistingWord := "newJoe"
+
+	err := dictionary.Update(notExistingWord, newDesc)
+
+	assertError(t, err, ErrMsgWordNotFound)
+}
+
+func TestDelete_ShouldDelete(t *testing.T) {
+	word := "joe"
+	desc := "momma"
+	dictionary := Dictionary{word: desc}
+
+	errDel := dictionary.Delete(word)
+
+	assertNoError(t, errDel)
+	_, errSearch := dictionary.Search(word)
+	assertError(t, errSearch, ErrMsgWordNotFound)
+}
+
+func TestDelete_ErrWhenDeleteNonExisting(t *testing.T) {
+	dictionary := Dictionary{"joe": "momma"}
+	notExistingWord := "mio"
+
+	err := dictionary.Delete(notExistingWord)
+
+	assertError(t, err, ErrMsgWordNotFound)
+}
+
+func assertEquals(t testing.TB, got, want string) {
 	t.Helper()
 	if got != want {
-		t.Errorf("got %q want %q given, %q", got, want, "test")
+		t.Errorf("got %q want %q given", got, want)
 	}
 }
 
-func AssertError(t testing.TB, err error, expectedMsg string) {
+func assertError(t testing.TB, err error, expectedMsg string) {
 	t.Helper()
 	if err == nil {
 		t.Fatal("expected error, got nil")
 	}
 	if err.Error() != expectedMsg {
 		t.Errorf("expected error %s, got %s", expectedMsg, err.Error())
+	}
+}
+
+func assertNoError(t *testing.T, err error) {
+	t.Helper()
+	if err != nil {
+		t.Fatal("got an error but didn't want one")
 	}
 }
